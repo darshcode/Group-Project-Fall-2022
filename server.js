b521776b92eda35c7375c56475d7a00ec94d6b3c
Alex Tang <alextoronto66@gmail.com>
2022-11-08 11:53:26 -0500

    Finished lab2 content, which is init npm, install connect package, the local web could show hello world now

---
+++ server.js
@@ -0,0 +1,26 @@
+// import third-party nodejs module Connect
+const connect = require('connect');
+
+// instantiate app-server
+const app = connect();
+
+// custom middleware
+function helloWorld(req, res, next){
+    res.setHeader('Content-Type','text/plain');
+    res.end('Hello World');
+};
+
+// custom middleware
+function byeWorld(req, res, next){
+    res.setHeader('Content-Type','text/plain');
+    res.end('Good Bye World');
+};
+
+// add middleware to connect application
+app.use('/hello',helloWorld);
+app.use('/bye', byeWorld);
+
+// run app
+app.listen(3000);
+
+console.log('Server running at http://localhost:3000');
