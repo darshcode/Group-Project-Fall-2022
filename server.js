953611f2995fb710bbcbc865400fc9e93c2ff29f
Darshmeen Dhillon <darsh.dhillon@live.com>
2022-11-10 15:44:40 -0500

    added app ussage, dir name and setup view engine.

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
 //This is test for merge branch to main
 
-// importing express using es6 expression import
+// importing express and third party modules using 3s6 format.
 import express from "express";
+import cookieParser from "cookie-parser";
+import logger from 'morgan';
+import session from "express-session";
+
+// es modules to fix __dirname
+import path, {dirname} from 'path';
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // instantiate app-server
 const app = express();
+
+//setup viewEngine EJS
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(session({
+    secret: 'itsASecret', saveUninitialized:false,
+    resave:false,
+}))
+
+
+
 
 // custom middleware
 function helloWorld(req, res, next){
