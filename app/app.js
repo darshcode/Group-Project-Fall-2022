4a09addfab4be84c57a526866444a5f4b55b69bf
Alex Tang <alextoronto66@gmail.com>
2022-11-11 13:55:11 -0500

    Mongoose Working and Surveys Model Completed

--- app/app.js
+++ app/app.js
@@ -9,14 +9,26 @@ import path, {dirname} from 'path';
 import { fileURLToPath } from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+//Import Mongoose Module
+import mongoose from 'mongoose';
+
+
 // Configuration Module
-import { Secret } from "../config/config.js";
+import { MongoURI, Secret } from "../config/config.js";
 
 // Import Router
 import indexRouter from './routes/index.route.server.js';
 
 // instantiate app-server
 const app = express();
+
+//Listen for connection success or error
+db.on('open', () => console.log('Connected to MongoDB'));
+db.on('error', () => console.log('Mongo Connection Error'));
+
+//Complete the DB Configuration
+mongoose.connect(MongoURI);
+const db = mongoose.connection;
 
 // setup ViewEngine EJS
 app.set('views', path.join(__dirname,'/views'));
