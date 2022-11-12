a36f00b927bd940da118eb423ebcb542061fe189
Alex Tang <alextoronto66@gmail.com>
2022-11-12 11:14:14 -0500

    survey content edit in developing

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,3 +9,12 @@
 
 })();
 
+//Show hidden add survey content div
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
