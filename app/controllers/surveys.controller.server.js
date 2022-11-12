81760db2c97ef98406986fdd111d168025ce2398
Alex Tang <alextoronto66@gmail.com>
2022-11-12 10:39:42 -0500

    copy from Alex branch. survey controller and router. index.ejs, header,ejs,

---
+++ app/controllers/surveys.controller.server.js
@@ -0,0 +1,78 @@
+import surveyModel from '../models/surveys.js';
+
+export function DisplaySurveysList(req, res, next){
+    surveyModel.find(function(err, surveysCollection) {
+        if(err){
+            console.error(err);
+            res.end(err);
+        }
+
+        res.render('index', {title: 'Survey List', page: 'surveys/list', surveys: surveysCollection});
+    })
+}
+
+export function DisplaySurveysAddPage(req, res, next){
+    res.render('index', { title: 'Add Survey', page: 'surveys/edit', survey: {} });
+}
+
+export function ProcessSurveysAddPage(req, res, next){
+    
+    let newSurvey = surveyModel({
+        test: req.body.test,
+        
+    });
+
+    surveyModel.create(newSurvey, (err, Survey) => {
+        if(err){
+            console.error(err);
+            res.end(err);
+        };
+
+        res.redirect('/survey-list')
+    } )
+}
+
+export function DisplaySurveysEditPage(req, res, next){
+    let id = req.params.id;
+
+    surveyModel.findById(id, (err, survey) => {
+        if(err){
+            console.error(err);
+            res.end(err);
+        }
+
+        res.render('index', { title: 'Edit Survey', page: 'surveys/edit', survey: survey });
+    });    
+}
+
+export function ProcessSurveysEditPage(req, res, next){
+
+    let id = req.params.id;
+    
+    let newSurvey = surveyModel({
+        _id: req.body.id,
+        test: req.body.test,
+    });
+
+    surveyModel.updateOne({_id: id }, newSurvey, (err, Survey) => {
+        if(err){
+            console.error(err);
+            res.end(err);
+        };
+
+        res.redirect('/survey-list')
+    } )
+}
+
+export function ProcessSurveysDelete(req, res, next){
+    let id = req.params.id;
+
+    surveyModel.remove({_id: id}, (err) => {
+        if (err){
+            console.error(err);
+            res.end(err);
+        }
+
+        res.redirect('/survey-list');
+    })
+}
