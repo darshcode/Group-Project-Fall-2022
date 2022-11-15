4d7af5754d2517d6d36b20f6c611441d497dfda3
LookyLuCU <113215900+LookyLuCU@users.noreply.github.com>
2022-11-15 03:17:14 -0800

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,3 +1,15 @@
+/*Web App Name: Take The Survey
+File Name: app.js;
+Group Name: Team A+;
+Date: November 10, 2022
+Student Name      Student ID
+Alex Tang         301238850
+Eva Fan           301238820
+Darshmeen Dhillon 300689994
+Faiyaz Muhammad   301227442
+Sheila Donnelly   822281549
+Wilson Wu         3012453494*/
+
 // Third Party Modules
 import express from "express";
 import cookieParser from "cookie-parser";
