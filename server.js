f68ce41df102a97133cd673cf4803c2b6ac87d30
LookyLuCU <113215900+LookyLuCU@users.noreply.github.com>
2022-11-15 03:23:06 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+/*Web App Name: Take The Survey
+File Name: index.controller.server.js;
+Group Name: Team A+;
+Date: November 10, 2022
+Student Name      Student ID
+Alex Tang         301238850
+Eva Fan           301238820
+Darshmeen Dhillon 300689994
+Faiyaz Muhammad   301227442
+Sheila Donnelly   822281549
+Wilson Wu         3012453494*/
+
 import debug from 'debug';
 debug('comp-229');
 import http from 'http';
