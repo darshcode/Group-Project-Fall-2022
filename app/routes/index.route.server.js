5820e8bba3b0b1375f203795edc79ef094ac7a9c
LookyLuCU <113215900+LookyLuCU@users.noreply.github.com>
2022-11-15 03:19:58 -0800

    Update index.route.server.js

--- app/routes/index.route.server.js
+++ app/routes/index.route.server.js
@@ -1,3 +1,15 @@
+/*Web App Name: Take The Survey
+File Name: index.route.server.js;
+Group Name: Team A+;
+Date: November 10, 2022
+Student Name      Student ID
+Alex Tang         301238850
+Eva Fan           301238820
+Darshmeen Dhillon 300689994
+Faiyaz Muhammad   301227442
+Sheila Donnelly   822281549
+Wilson Wu         3012453494*/
+
 // Index Routes
 //importing router from express and importing functinos from controller section.
 import { Router } from 'express';
