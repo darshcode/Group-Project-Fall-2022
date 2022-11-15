ff4304655cedf6c4215135b6220b20c01a4961ad
LookyLuCU <113215900+LookyLuCU@users.noreply.github.com>
2022-11-15 03:16:38 -0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,2 +1,14 @@
+/*Web App Name: Take The Survey
+File Name: config.js;
+Group Name: Team A+;
+Date: November 10, 2022
+Student Name      Student ID
+Alex Tang         301238850
+Eva Fan           301238820
+Darshmeen Dhillon 300689994
+Faiyaz Muhammad   301227442
+Sheila Donnelly   822281549
+Wilson Wu         3012453494*/
+
 export const Secret = "MySecret";
 export const MongoURI = "mongodb+srv://WebDevAlex:7wYdpy5IY2bHySlN@cluster0.zkqspsk.mongodb.net/?retryWrites=true&w=majority"
