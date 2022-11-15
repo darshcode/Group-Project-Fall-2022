9162f3ca77cc1ac20857bab2186f8505a8c9e221
LookyLuCU <113215900+LookyLuCU@users.noreply.github.com>
2022-11-15 03:20:42 -0800

    Update surveys.controller.server.js

--- app/controllers/surveys.controller.server.js
+++ app/controllers/surveys.controller.server.js
@@ -1,3 +1,15 @@
+/*Web App Name: Take The Survey
+File Name: surveys.controller.server.js;
+Group Name: Team A+;
+Date: November 10, 2022
+Student Name      Student ID
+Alex Tang         301238850
+Eva Fan           301238820
+Darshmeen Dhillon 300689994
+Faiyaz Muhammad   301227442
+Sheila Donnelly   822281549
+Wilson Wu         3012453494*/
+
 import surveyModel from '../models/surveys.js';
 
 export function DisplaySurveysList(req, res, next){
