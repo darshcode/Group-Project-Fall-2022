7c7bb1348a62888b6e02d274c8dc981e31fb1b2b
Alex Tang <alextoronto66@gmail.com>
2022-11-22 21:46:26 -0500

    Comment to all documents

--- app/utils/index.js
+++ app/utils/index.js
@@ -1,3 +1,15 @@
+/*Web App Name: Take The Survey
+File Name: index.controller.server.js;
+Group Name: Team A+;
+Date: November 10, 2022
+Student Name      Student ID
+Alex Tang         301238850
+Eva Fan           301238820
+Darshmeen Dhillon 300689994
+Faiyaz Muhammad   301227442
+Sheila Donnelly   822281549
+Wilson Wu         301243494*/
+
 export function UserDisplayName(req){
     if(req.user){
         return req.user.displayName;
