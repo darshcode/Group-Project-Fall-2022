2f485351db0fca94e22173874eae84064c99b7ca
LookyLuCU <113215900+LookyLuCU@users.noreply.github.com>
2022-12-02 02:37:37 -0800

    Create responses.js
    added responses model

---
+++ app/models/responses.js
@@ -0,0 +1,27 @@
+/*Web App Name: Take The Survey
+File Name: responses.js;
+Group Name: Team A+;
+Date: November 26, 2022
+Student Name      Student ID
+Alex Tang         301238850
+Eva Fan           301238820
+Darshmeen Dhillon 300689994
+Faiyaz Muhammad   301227442
+Sheila Donnelly   822281549
+Wilson Wu         301243494*/
+
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const responseSchema = new Schema({
+    responseDescription
+
+    // Survey content is TBD
+
+},{
+    timestamps: true,
+    collection:'responses'
+});
+
+export default mongoose.model('Response', responseSchema);
