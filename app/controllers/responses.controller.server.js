78c242516e2d2a2be362543cf65fc274cd0f9919
LookyLuCU <113215900+LookyLuCU@users.noreply.github.com>
2022-12-02 02:31:35 -0800

    Create responses.controller.server.js
    Added responses controller

---
+++ app/controllers/responses.controller.server.js
@@ -0,0 +1,61 @@
+/*Web App Name: Take The Survey
+File Name: responses.controller.server.js;
+Group Name: Team A+;
+Date: November 10, 2022
+Student Name      Student ID
+Alex Tang         301238850
+Eva Fan           301238820
+Darshmeen Dhillon 300689994
+Faiyaz Muhammad   301227442
+Sheila Donnelly   822281549
+Wilson Wu         301243494*/
+
+import questionModel from '../models/questions.js';
+import surveyModel from '../models/surveys.js';
+import responsesModel from '../models/responses.js';
+
+
+// import DisplayName Utility method
+import { UserDisplayName } from '../utils/index.js';
+
+// Index Controller
+//this section exports the function needed to render pages during click events. 
+//these function are used by:       routes --> index.Route.server.js
+//this code is related to views
+//the res part of res.render mean response.  the render method renders the index file under index.ejs 
+//and uses {title and pages under components.} so we have two parameters.. 
+//also notice that the part in {curly braces is in jason format}.
+//finally notice the page: home. this is another variable were passing to index.ejs in the main article. by using this we can refer to the right page.
+
+export function DisplayResponsesList(req, res, next){
+    responsesModel.find(function(err, responsesCollection) {
+        if(err){
+            console.error(err);
+            res.end(err);
+        }
+
+        res.render('index', {title: 'Responses List', page: 'responses/list', responses: responsesCollection, displayName: UserDisplayName(req)});
+    })
+}
+
+export function DisplayResponsesAddPage(req, res, next){
+    res.render('index', { title: 'Add Response', page: 'response/edit', survey : {} ,response: {}, displayName: UserDisplayName(req) });
+}
+
+export function ProcessQuestionsAddPage(req, res, next){
+    
+    let newResponse = responseModel({
+        responseDescription : req.body.questionDescription,
+        
+        
+    });
+
+    responseModel.create(newResponse, (err, Response) => {
+        if(err){
+            console.error(err);
+            res.end(err);
+        };
+
+        res.redirect("/response-list/:id")
+    } )
+}
