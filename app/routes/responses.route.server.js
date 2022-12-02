ede4a5dd3cbb18fc1f17e0308299ecb38c177b36
LookyLuCU <113215900+LookyLuCU@users.noreply.github.com>
2022-12-02 02:19:09 -0800

    Update responses.route.server.js

--- app/routes/responses.route.server.js
+++ app/routes/responses.route.server.js
@@ -1,1 +1,35 @@
+/*Web App Name: Take The Survey
+File Name: responses.route.server.js;
+Group Name: Team A+;
+Date: December 1st, 2022
+Student Name      Student ID
+Alex Tang         301238850
+Eva Fan           301238820
+Darshmeen Dhillon 300689994
+Faiyaz Muhammad   301227442
+Sheila Donnelly   822281549
+Wilson Wu         301243494*/
 
+
+//Index Routes
+//importing router from express.
+import { Router } from "express";
+
+//Importing functinos from 'index.controller.server' from controllers.
+import {DisplayQuestionsList,
+    DisplayQuestionsAddPage,
+    ProcessQuestionsAddPage 
+     } from "../controllers/questions.controller.server.js";
+import { AuthGuard } from "../utils/index.js";
+
+    // Intanciating the router from the imported package above
+const router = Router();
+
+//add middleware
+router.get('/responses-list/:id', DisplayResponsesList);
+router.get('/responses-add/:id', AuthGuard, DisplayREsponsessAddPage);
+router.post('/responses-add/:id', AuthGuard, ProcessResponsesAddPage);
+
+
+//this router is now exportable and can be used.
+export default router;
